155aa2c863a8a90d91ca6c9f38a8e18280bec97e
Phuong Nguyen <phuongnd08@gmail.com>
2017-07-08 09:24:22 +0700

    Get resque-web injected

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
+Resque.redis = ENV["REDIS_RESQUE"] || "redis://localhost:6379"
+Resque.redis.namespace = ENV["REDIS_RESQUE_NAMESPACE"] if ENV["REDIS_RESQUE_NAMESPACE"]
+
+ENV['RESQUE_WEB_HTTP_BASIC_AUTH_USER'] = ENV['RESQUE_USER'] || "admin"
+ENV['RESQUE_WEB_HTTP_BASIC_AUTH_PASSWORD'] = ENV['RESQUE_PASSWORD'] || "secret"
+
+require "resque_web"
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  mount ResqueWeb::Engine => "/"
 end
